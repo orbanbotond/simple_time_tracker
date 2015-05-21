e09cbed42e1380d19c06adf495eb7af9c37c7362
Orban Botond <orbanbotond@gmail.com>
2015-05-21 16:22:30 +0300

    --amend

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( livereload.js )
+Rails.application.config.assets.precompile += %w( livereload.js swagger_ui.js )
 
-Rails.application.config.assets.precompile += %w( non-responsive.css )
+Rails.application.config.assets.precompile += %w( non-responsive.css swagger_ui.css )
