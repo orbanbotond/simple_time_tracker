3254b0e2f69143c5464f5ee482e5f22db3bf2c35
Orban Botond <orbanbotond@gmail.com>
2015-05-25 15:29:54 +0300

    Another validation is included into the filter form object

--- app/form_objects/filter_work_sessions.rb
+++ app/form_objects/filter_work_sessions.rb
@@ -4,7 +4,26 @@ class FilterWorkSessions
   attribute :from, type: Date
   attribute :to, type: Date
 
+  validate :start_time_is_before_end_time
+  validate :end_time_is_not_in_the_future
+
   def filtering?
     from.present? && to.present?
   end
+
+  private
+
+  def start_time_is_before_end_time
+    return unless from.present?
+    return unless to.present?
+
+    errors.add(:from, 'to must be after from') if from > to
+  end
+
+  def end_time_is_not_in_the_future
+    return unless to.present?
+
+    errors.add(:to, 'to can\'t be in the future') if to > Time.zone.now
+  end
+
 end
--- spec/form_objects/filter_work_sessions_spec.rb
+++ spec/form_objects/filter_work_sessions_spec.rb
@@ -5,15 +5,35 @@ describe FilterWorkSessions do
     it { is_expected.to respond_to(:from) }
     it { is_expected.to respond_to(:to) }
 
-    context "typecasting" do
+    context 'typecasting' do
       specify 'should converyt to date' do
         expect(FilterWorkSessions.new(from: '01/02/2015').from).to be_a(Date)
       end
     end
   end   
 
-  context "methods" do
-    context "#filtering?" do
+  context 'validations' do
+    context 'validates that the from is before the to' do
+      let(:filter) { FilterWorkSessions.new from: '22/05/2014', to: '21/05/2014' }
+
+      specify 'should be invalid and contain the proper error message' do
+        expect(filter).to_not be_valid
+        expect(filter.errors[:from]).to include('to must be after from')
+      end
+    end
+
+    context 'validates that the to is not in the future' do
+      let(:filter) { FilterWorkSessions.new from: '22/05/2014', to: 1.day.from_now }
+
+      specify 'should be invalid and contain the proper error message' do
+        expect(filter).to_not be_valid
+        expect(filter.errors[:to]).to include('to can\'t be in the future')
+      end
+    end
+  end
+
+  context 'methods' do
+    context '#filtering?' do
       it { is_expected.to respond_to(:filtering?) }
 
       specify 'returns true if the from and to are present' do
